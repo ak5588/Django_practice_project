5804d7389199e3e807340d4af9de42e19a0a6fbb
Alok Kushwaha <alokkushwaha881@gmail.com>
2025-08-31 14:48:22 +0530

    Models and forms are builded

--- pract_tweeter/tweet/models.py
+++ pract_tweeter/tweet/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tweet(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    text=models.TextField(max_length=250)
+    photo=models.ImageField(upload_to="photos/",blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(Self):
+        return f'{Self.user.username}-{Self.text[:10]}'
+
